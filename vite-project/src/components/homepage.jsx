961c6de774220431d6e50b6740b3a0fa22cb488c
naveenkumaranand13 <naveenkumaranand13@gmail.com>
2026-04-30 14:57:42 +0530

    your message

---
+++ vite-project/src/components/homepage.jsx
@@ -0,0 +1,24 @@
+import React, {useState} from 'react'
+
+function Contact() {
+    const [name,setName]=useState('')
+    const [email,setEmail]=useState('')
+    const [message,setMessage]=useState('')
+
+    const handleSubmit=()=>{
+        event.preventDefault()
+        console.log(name,email,message)
+    }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder='Enter your name' value={name} onChange={(e)=>setName(e.target.value)}/>
+        <input type="email" placeholder='Enter your email' value={email}  onChange={(e)=>setEmail(e.target.value)}/>
+        <textarea placeholder='Enter your message' value={message}  onChange={(e)=>setMessage(e.target.value)}/>
+        <button type='submit'>Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default Contact
